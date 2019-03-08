4c4c8b9c144f1873fb32095209613cc58ce915a6
wicak <wicaksanaigede@gmail.com>
2019-03-08 09:41:29 +0800

    setting app.js

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.post("/image-upload", (req, res) => {
 });
 
 // serve static front end
-app.use('/', express.static(path.join(__dirname, 'dist')));
+app.use('/', express.static(path.join(__dirname, 'build')));
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, './build/index.html'));
 });
